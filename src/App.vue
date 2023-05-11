35edd45eb3b65119c36564d8da1e369451f412fd
YinCheng <eason010630@gmail.com>
2023-05-11 11:45:27 +0800

    0.2.0

--- src/App.vue
+++ src/App.vue
@@ -3,24 +3,11 @@ import home from './components/home.vue'
 </script>
 
 <template>
-  <h1 class="team_name">YINLA</h1>
   <home></home>
 </template>
 
 <style scoped>
-  .team_name{
-    font-size: 60px;
-    animation: openIn 1s ease-in-out; 
-  }
+  
 
-  @keyframes openIn{
-        0% {
-            opacity: 0;
-        }
-        100% {
-            opacity: 1;
-        }
-        
-
-    }
+  
 </style>
--- src/components/home.vue
+++ src/components/home.vue
@@ -1,39 +1,74 @@
 <template>
+    <h1 class="team_name" style="moveUp">YINLA</h1>
+    <div v-if="view === 1">
+        <button @click="changeView(2)">進入</button>
+    </div>
 
-<div v-if="view === 1">
-    <button @click="changeView(2)">進入</button>
+    <div v-if="view === 2">
+        <button @click="changeView(3)">簡介</button>
+        <button @click="changeView(4)">社群</button>
+        <button @click="changeView(1)">返回</button>
+    </div>
 
-        
-</div>
+    <div v-if="view === 3">
+        <img id="yinla_logo" src="/106425898.png" alt="YINLA_LOGO">
+        <h3 class="info">創立於 2022年<br>創辦人 <a  href="https://www.instagram.com/_yincheng_/" target="_blank">YinCheng</a></h3>
+        <button @click="changeView(2)">返回</button>
+    </div>
 
-<div v-if="view === 2">
-    <button @click="changeView(3)">簡介</button>
-    <button @click="changeView(4)">社群</button>
-    <button @click="changeView(1)">返回</button>
-</div>
-
-<div v-if="view === 3">
-    <p>balabala</p>
-    <button @click="changeView(2)">返回</button>
-</div>
-
-<div v-if="view === 4">
-    <button onclick="location.href='https://discord.gg/mnCHdBbh65'">Discord</button>
-    <button onclick="location.href='https://github.com/YINLA-TEAM'">GitHub</button>
-    <button>other</button>
-    <br> <br>
-    <button @click="changeView(2)">返回</button>
-</div>
-
+    <div v-if="view === 4">
+        <p>點選以下按鈕帶你到我們的社群</p>
+        <button onclick="location.href='https://discord.gg/mnCHdBbh65'">
+            Discord
+        </button>
+        <button onclick="location.href='https://github.com/YINLA-TEAM'">
+            GitHub
+        </button>
+        <button>other</button>
+        <br />
+        <br />
+        <button @click="changeView(2)">返回</button>
+    </div>
 </template>
 
 <script setup>
-import { ref } from 'vue'
-const view = ref(1)
-    const changeView = (index) => {
-        view.value = index
+import { reactive, ref, watch } from "vue";
+const view = ref(1);
+const changeView = (index) => {
+    view.value = index;
+};
+const moveUp = reactive({
+    fonSize : '60px',
+    animation : 'openIn 1s ease-in-out',
+    transform: 'translateY(0)'
+})
+
+watch(view.value, () => {
+    if(view.value === 2 || 3 || 4){
+        moveUp.transform = 'translateY(20px)'
     }
+})
 </script>
 
 <style scoped>
+#yinla_logo {
+    border-radius: 45px;
+    animation: openIn 1.5s ease-in-out;
+}
+
+button, .info, h1 {
+    animation: openIn 1.5s ease-in-out;
+}
+
+
+@keyframes openIn{
+        0% {
+            opacity: 0;
+        }
+        100% {
+            opacity: 1;
+        }
+    }
+
+
 </style>
